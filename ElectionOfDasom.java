d0859e55b17c7c7d12822c44b3db449401793d8b
junheeee8462 <jjangjun1001@naver.com>
2024-10-06 12:57:14 +0900

    다솜이의 선거 최종 코드

--- ElectionOfDasom.java
+++ ElectionOfDasom.java
@@ -1,40 +1,43 @@
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class ElectionOfDasom {
-    public static void main(String[] args) {
-        
-        System.out.println(" 전체 후보자 수와 각 후보자의 득표수를 입력하시오 ");
-
-          Scanner sc = new Scanner(System.in);
-       int N = sc.nextInt(); // 전체 후보자의 수 입력받기
     
-       int [] cd = new int [N]; //N 명의 후보자 만큼 후보자를 입력받을 배열
-       int sum = 0; // 매수를 위해 필요한 표의 수를 저장하기 위해 사용하는 변수
+    public static void main(String[] args) {
+       
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; i<cd.length; i++){ // 입력받은 전체 후보자의 수만큼 후보자의 득표수 입력받기
-            cd[i] = sc.nextInt();
-            
-            for (int j = 1; j<cd.length; j++){
+        System.out.println(" 전체 후보자의 수를 입력한 뒤, 각 후보자의 투표수를 입력하시오 ");
+		int N = sc.nextInt(); //전체 후보자의 수 입력
+		int Dasom = sc.nextInt(); //다솜이의 득표수 따로 받기
+		Integer cd[] = new Integer[N-1]; // 이후 내림 차순 정렬을 받기 위해 Integer 타입의 배열 선언
+		int sum = 0; //매수해야 하는 사람 수
 
-               if (cd[0] < cd[j] ){
-                    cd[0] +=1;
-                    cd[j] -=1;
-                    sum ++;                     
-                }else if ((cd[j] - cd[0]) ==1){
-                    cd[0] +=1;
-                    cd[j] -=1;
-                    sum++;
-                }
-               
-            }
-                  
+		for(int i = 0; i < cd.length; i++){ // 다솜이의 투표수를 제외한 전체 후보자의 투표수를 입력받음
+             cd[i] = sc.nextInt();
         }
-        System.out.println("--------");
 
-        for (int u =0; u<cd.length; u++){
-            System.out.println(cd[u]);
-        }
-        System.out.println( "매수해야하는 표 수: "+ sum);
+		for (int j = 0; j < cd.length; j++) // 다솜이를 제외한 후보자들의 투표수를 비교하기 위한 반복문 
+		{
+			Arrays.sort(cd, Collections.reverseOrder()); // 다솜이를 제외한 후보자들의 투표수를 내림 차순 정렬
+			if(N == 1 || cd[0] < Dasom) { 
+                // 만약 전체 후보자가 다솜이 혼자이거나, 전체 후보자 중 가장 득표수가 많은 사람 (cd[0]) 이 다솜이보다 득표수가 낮다면 반복문 바로 중단
+                break; 
+            }   
+			else 
+			{
+				Dasom++;
+				cd[0]--;
+				sum++;
+                
+			}
+		}
+        System.out.println("----------매수 투표 결과----------");
+        System.out.println("다솜이의 투표수: " + Dasom);
+        System.out.println("2번 ~ " + N + " 번 후보자의 투표수: "+  Arrays.toString(cd));
+		System.out.println("다솜이가 매수해야할 투표수: " + sum);
+
     }
 
 }
